d3db6a115cb9b2921d6b10f76a68c836275b65d4
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-25 14:43:02 -0400

    Submit button changes heading to text box content

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const button = document.querySelector('button');
+const button = document.querySelector('#submitChris');
+
+document.querySelector("#submitChris").addEventListener("click", function(event) {
+    document.getElementById("submitChris");
+    event.preventDefault();
+}, false);
 
 const changeHeader = function() {
-    const p = document.querySelector('#chris');
-    p.textContent = 'Beware of Chris';
+    const p = document.querySelector('h1');
+    p.textContent = document.getElementById('textChris').value;
 }
 
 button.addEventListener('click', changeHeader);
