cd0bb9739afad7e56d83170264a3b80952f33a0d
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-26 15:25:08 -0400

    Use multiple functions to display words

--- index.js
+++ index.js
@@ -4,31 +4,24 @@ const createLists = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
-  //addToDivs(f.flickName.value, f.actorName.value)
+  const flick = addToElement(f.flickName.value)
+  const actor = addToElement( f.actorName.value)
 
-  const flick = f.flickName.value 
-  const actor =  f.actorName.value
-
-  const item = document.createElement('div')
-  item.textContent += flick
-
-  const items = document.createElement('div')
-  items.textContent += actor
-
-  const list = document.querySelector('#flicks')
-  const lists = document.querySelector('#actors')
-
-  list.appendChild(item)
-  lists.appendChild(items)
-
+  append('#flicks', flick)
+  append('#actors', actor)
+ 
   f.reset()
 }
 
-const addToDivs = function(flick, actor) {
-    const item = document.createElement('li')
-    item.textContent += flick
-
-    const list = document.querySelector('#flicks')
-    list.appendChild(item)
+const addToElement = function(word) {
+    const item = document.createElement('div')
+    item.textContent += word
+    return item
 }
+
+const append = function(place, word) {
+    const list = document.querySelector(place)
+    list.appendChild(word)
+}
+
 form.addEventListener('submit', createLists)
