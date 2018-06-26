e76f05a84e2a71d56306127dc1b67504764e7bf4
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-26 15:06:37 -0400

    Add items to divs using createElement and appendChild

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const form = document.querySelector('form#flickForm')
 
-const createList = function(ev) {
+const createLists = function(ev) {
   ev.preventDefault()
   const f = ev.target
-    
-  const flicksDiv = document.querySelector('#flicks')
-  flicksDiv.textContent += ' ' + f.flickName.value
 
-  const actorDiv = document.querySelector('#actors')
-  actorDiv.textContent += ' ' + f.actorName.value
+  //addToDivs(f.flickName.value, f.actorName.value)
+
+  const flick = f.flickName.value 
+  const actor =  f.actorName.value
+
+  const item = document.createElement('div')
+  item.textContent += flick
+
+  const items = document.createElement('div')
+  items.textContent += actor
+
+  const list = document.querySelector('#flicks')
+  const lists = document.querySelector('#actors')
+
+  list.appendChild(item)
+  lists.appendChild(items)
 
   f.reset()
 }
 
-form.addEventListener('submit', createList)
+const addToDivs = function(flick, actor) {
+    const item = document.createElement('li')
+    item.textContent += flick
+
+    const list = document.querySelector('#flicks')
+    list.appendChild(item)
+}
+form.addEventListener('submit', createLists)
