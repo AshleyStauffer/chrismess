d0e081d27f281594b7ac708ee15b2a13532186f9
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-26 14:36:21 -0400

    Display fields in separate divs with style

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const form = document.querySelector('form#flickForm')
 const createList = function(ev) {
   ev.preventDefault()
   const f = ev.target
+    
+  const flicksDiv = document.querySelector('#flicks')
+  flicksDiv.textContent += ' ' + f.flickName.value
 
-  const flickName = f.flickName.value + f.actorName.value
-
-  const item = document.createElement('li')
-  item.textContent += flickName
-
-  const list = document.querySelector('#flicks')
-  list.appendChild(item)
+  const actorDiv = document.querySelector('#actors')
+  actorDiv.textContent += ' ' + f.actorName.value
 
   f.reset()
 }
