3db147c60382a42396e080cd8ae8d6112d86dd82
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-26 12:51:11 -0400

    Add field's value to list

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const createList = function(ev) {
   ev.preventDefault()
   const f = ev.target
 
-  const flickName = f.flickName.value
+  const flickName = f.flickName.value + f.actorName.value
+
   const item = document.createElement('li')
-  item.textContent = flickName
+  item.textContent += flickName
 
   const list = document.querySelector('#flicks')
   list.appendChild(item)
