02616f713e8bd2f1d308b0f4ecd81047d2551338
Ashley Stauffer <anstauffer21@gmail.com>
2018-06-28 18:40:12 -0400

    Keep flicks and actors list when page refreshed

--- index.js
+++ index.js
@@ -1,87 +1,121 @@
 class App {
     constructor() {
+      this.list = document.querySelector('#flicks')
+
+      this.flicks = []
+      this.load()
+      
       const form = document.querySelector('form#flickForm')
-       this.flicks = []
       form.addEventListener('submit', (ev) => {
         ev.preventDefault()
         this.handleSubmit(ev)
       })
     }
+
+    save() {
+        // store flicks array in localStorage
+        localStorage.setItem('flicks', JSON.stringify(this.flicks))
+    }
   
+    load() {
+        const flicks = JSON.parse(localStorage.getItem('flicks'))
+        
+        if (flicks) {
+            flicks.forEach(flick => this.addFlick(flick))
+        }
+    }
     renderProperty(name, value) {
-      const span = document.createElement('span')
-      span.classList.add(name)
-      span.textContent = value + ' '
-      return span
+        const span = document.createElement('span')
+        span.classList.add(name)
+        span.textContent = value + ' '
+        return span
     }
   
+    renderActionButtons(flick, item) {
+        // add a favorite button 
+        const fav = document.createElement('button')
+        item.appendChild(fav)
+        if(flick.favorite) {
+            fav.classList.add('favOn')
+        }
+        fav.classList.add('fav')
+        fav.innerHTML = '<i class="fas fa-star" title="Favorite"></i>'
+
+        // make favorite button functional
+        fav.addEventListener('click', () => this.toggleFavorite(flick, item, fav)) 
+        
+        // add a delete button
+        const del = document.createElement('button')
+        item.appendChild(del)
+        del.classList.add('delete')
+        del.innerHTML = '<i class="far fa-trash-alt" title="Delete flick"></i>'
+
+        // make delete button functional
+        del.addEventListener('click', (_ev) => this.removeFlick(flick, item))
+        return fav;
+    }
+
+    renderProperties(flick, item) {
+        // get the list of properties
+        const properties = Object.keys(flick)
+    
+        // loop over the properties
+        properties.forEach((propertyName) => {
+            // build a span, and append it to the list
+            const span = this.renderProperty(propertyName, flick[propertyName])
+            item.appendChild(span)
+        })
+    }
+
     renderItem(flick) {
-      const item = document.createElement('li')
-  
-      // get the list of properties
-      const properties = Object.keys(flick)
-  
-      // loop over the properties
-      properties.forEach((propertyName) => {
-        // build a span, and append it to the list
-        const span = this.renderProperty(propertyName, flick[propertyName])
-        item.appendChild(span)
-      })
-      return item
+        const item = document.createElement('li')
+        
+        this.renderProperties(flick, item)
+        this.renderActionButtons(flick, item)
+        
+        return item;
     }
-  
+
+    toggleFavorite(flick, item, fav) {
+        flick.favorite = item.classList.toggle('favorite')
+        fav.classList.toggle('favOn')
+        
+        this.save()
+    }
+    
+    removeFlick(flick, item) {
+        this.list.removeChild(item)
+
+        const i = this.flicks.indexOf(flick)
+        this.flicks.splice(i, 1)
+
+        this.save()
+    }
+
+    addFlick(flick) {
+        this.flicks.push(flick)
+        
+        const item = this.renderItem(flick)
+        if(flick.favorite) {
+            item.classList.add('favorite')
+        }
+        this.list.appendChild(item)
+    }
+
     handleSubmit(ev) {
       const f = ev.target
   
       const flick = {
         movie: f.flickName.value + ': ',
         chris: f.chrisName.value,
+        favorite: false
       }
       
-      this.flicks.push(flick)
+      this.addFlick(flick)
 
-      const item = this.renderItem(flick)
-  
-      const list = document.querySelector('#flicks')
-
-      // add a favorite button 
-      const fav = document.createElement('button')
-      const like = document.createTextNode('Favorite')
-      fav.appendChild(like)
-      item.appendChild(fav)
-      fav.classList.add('fav')
-      
-      // make favorite button functional
-      fav.addEventListener('click', () => {
-          item.classList.toggle('favorite')
-          fav.classList.toggle('favOn')
-          let i = this.flicks.indexOf(flick)
-          if(flick.favorite == 'YES') {
-            flick.favorite = 'NO'
-          }
-          else {
-               flick.favorite = 'YES'
-          }
-          
-      })
-
-      list.appendChild(item)
-
-      // add a delete button
-      const del = document.createElement('button')
-      const word = document.createTextNode('REMOVE')
-      del.appendChild(word)
-      item.appendChild(del)
-      del.classList.add('delete')
-
-      // make delete button functional
-      del.addEventListener('click', () => {
-        let i = this.flicks.indexOf(flick)
-        this.flicks.splice(i, 1)
-        list.removeChild(item)
-      })
       f.reset()
       f.flickName.focus()
+      this.save()
     }
 
   }
